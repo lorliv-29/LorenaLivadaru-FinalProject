85caf46b2bf337afd4d0f61479acab579f911e14
lorliv-29 <livadaru.design@gmail.com>
2025-10-12 17:01:04 +0200

    Update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     public GameObject projectilePrefab;
+    public float speed = 10f;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -27,4 +28,27 @@ public class PlayerController : MonoBehaviour
 
         }
     }
+
+    void FixedUpdate()
+    {
+        // Get input axes
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        // Create a movement vector
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        // Apply the movement to the Rigidbody
+        rb.AddForce(movement * speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            // Increase the player's size by 10%
+            transform.localScale *= 1.1f;
+            // Destroy the power-up object
+            Destroy(other.gameObject);
+        }
+    }
+
 }
+
