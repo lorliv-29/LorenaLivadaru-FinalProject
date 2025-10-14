ae20604d339d2cd2906ac825029372ebf2e34e98
lorliv-29 <livadaru.design@gmail.com>
2025-10-14 17:02:17 +0200

    sticky

---
+++ Assets/Scripts/Sticky.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Sticky : MonoBehaviour
+{
+    public float pushSpeed = 2f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            
+                // Push in the wall's up direction
+                Vector3 pushDirection = transform.up;
+                rb.AddForce(pushDirection * pushSpeed, ForceMode.Force);          
+        }
+    }
+}
