406e14e2a12091e52b284f3915c2f5e6b4c19658
lorliv-29 <livadaru.design@gmail.com>
2025-10-16 13:08:35 +0200

    update

--- Assets/Scripts/ObstacleManager.cs
+++ Assets/Scripts/ObstacleManager.cs
@@ -2,10 +2,25 @@ using UnityEngine;
 
 public class ObstacleManager : MonoBehaviour
 {
+    public GameObject[] obstaclePrefabs; // Array to obstacle prefabs
+    public Transform[] spawnPositions; // Array to spawn positions
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //Call reset method to randomize obstacles at start
+        ResetObstacles();
+    }
+
+    public void ResetObstacles()
+    {
+
+        Debug.Log("ResetObstacles() was called!");
+
+        for (int i = 0; i < obstaclePrefabs.Length && i < spawnPositions.Length; i++)
+        {
+            Instantiate(obstaclePrefabs[i], spawnPositions[i].position, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
