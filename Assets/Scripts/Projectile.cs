5a3647ca118da2aa8378aef58b7b5a47eb7e322b
lorliv-29 <livadaru.design@gmail.com>
2025-10-17 19:26:45 +0200

    DestructibleWall

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public GameObject BrickHitFBXPrefab; // Assign in Inspector
     void OnCollisionEnter(Collision col)
     {
         // Check if we hit a brick
         if (col.collider.CompareTag("Brick"))
         {
+            if (BrickHitFBXPrefab != null)
+            {
+
+                Vector3 vfxPosition = col.transform.position;
+                vfxPosition.y = 1f;
+                // spawn it at the brick's position
+                Instantiate(BrickHitFBXPrefab, vfxPosition, Quaternion.Euler(90f, 0f, 0f));
+            }
+
             Destroy(col.collider.gameObject); // Destroy the brick
+
+            // Always destroy the projectile on collision
+            Destroy(gameObject);
         }
 
-        // Always destroy the projectile on collision
-        Destroy(gameObject);
+    
     }
 }
