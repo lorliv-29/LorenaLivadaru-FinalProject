2b13f3f1a8f84cb65b8acf4367e7d51f655e27e1
lorliv-29 <livadaru.design@gmail.com>
2025-10-17 19:45:05 +0200

    Destructible Wall

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public GameObject BrickHitFBXPrefab; // Assign in Inspector
+    public float floatForce = 2f;
     void OnCollisionEnter(Collision col)
     {
         // Check if we hit a brick
@@ -17,12 +18,37 @@ public class Projectile : MonoBehaviour
                 Instantiate(BrickHitFBXPrefab, vfxPosition, Quaternion.Euler(90f, 0f, 0f));
             }
 
+            // Find the "InnerBricks" group in the scene
+            GameObject innerGroup = GameObject.Find("InnerBricks");
+
+            if (innerGroup != null)
+            {
+                // Apply upward force to each inner brick
+                foreach (Transform t in innerGroup.transform)
+                {
+                    // Check if the inner brick has the correct tag
+                    if (t.CompareTag("InnerBrick"))
+                    {
+                        Rigidbody rb = t.GetComponent<Rigidbody>();
+
+                        // Apply upward force
+                        if (rb != null)
+                        {
+                            rb.useGravity = true;
+                            rb.AddForce(Vector3.up * floatForce, ForceMode.Impulse);
+                        }
+                    }
+                }
+
+              
+            }
+
             Destroy(col.collider.gameObject); // Destroy the brick
 
             // Always destroy the projectile on collision
             Destroy(gameObject);
-        }
 
-    
+
+        }
     }
 }
