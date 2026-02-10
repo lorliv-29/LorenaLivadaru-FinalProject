444e457b7e1b435cfc513665f95e90bb6d81f926
Lorena Livadaru <livadaru.design@gmail.com>
2026-02-10 15:25:04 +0100

    errors

---
+++ Assets/Scripts/VRLever.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class VRLever : MonoBehaviour
+{
+    HingeJoint hinge;
+    public float leverOutput;
+    public float minValue, maxValue;
+    public float startingValue;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        hinge = GetComponent
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
